9665f45fd723c31ee9b08784e7c89d2485c8c4a0
Geming400 <Geming400@gmail.com>
2026-05-07 20:28:27 +0200

    Initial commit v2 bumped to 1.21.11 + modified mod id

---
+++ src/client/java/fr/geming400/pesticide/client/PesticidesDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.geming400.pesticide.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PesticidesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
