c4adf2802b67cfb79562bb2941517bca6e06d230
Geming400 <Geming400@gmail.com>
2026-05-15 12:32:11 +0200

    Fixed bug where bad farmer effect would only get applied when wearing the hazmat suit

--- src/main/java/fr/geming400/pesticide/content/blocks/InfestedFarmBlock.java
+++ src/main/java/fr/geming400/pesticide/content/blocks/InfestedFarmBlock.java
@@ -42,7 +42,7 @@ public class InfestedFarmBlock extends FarmBlockWithEntity {
     @Override
     @SuppressWarnings("UnstableApiUsage")
     public void stepOn(@NonNull Level level, @NonNull BlockPos blockPos, @NonNull BlockState blockState, @NonNull Entity entity) {
-        if (!level.isClientSide() && entity instanceof LivingEntity livingEntity && ModItems.HAZMAT_SUIT.isWearing(livingEntity)) {
+        if (!level.isClientSide() && entity instanceof LivingEntity livingEntity && !ModItems.HAZMAT_SUIT.isWearing(livingEntity)) {
             int timeSpentOnInfestedFarmland = entity.getAttachedOrCreate(ModAttachments.TIME_SPENT_ON_INFESTED_FARMLAND) ;
             entity.setAttached(ModAttachments.TIME_SPENT_ON_INFESTED_FARMLAND, timeSpentOnInfestedFarmland + 1);
 
